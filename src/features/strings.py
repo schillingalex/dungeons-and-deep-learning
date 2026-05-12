d626a423b99c7d7a1aff1d4ab508365a635c9947
Alexander Schilling <mail@alexanderschilling.de>
2026-05-12 14:46:00 +0200

    Add whitespace removal to character_vocabulary_from_dataset

--- src/features/strings.py
+++ src/features/strings.py
@@ -1,16 +1,23 @@
+import re
 
 
-def character_vocabulary_from_dataset(dataset: list[str], minimum_set: list[str] = None) -> list[str]:
+def character_vocabulary_from_dataset(dataset: list[str], minimum_set: list[str] = None,
+                                      include_whitespace: bool = False) -> list[str]:
     """
     Extracts the unique characters present in all samples of the given dataset.
 
     :param dataset: List of strings representing the samples.
     :param minimum_set: List of characters to include in the vocabulary even if they are missing from the dataset.
+        Optional, default: None.
+    :param include_whitespace: Determines whether to include whitespace characters in the vocabulary. Optional,
+        default: False.
     :return: Sorted list of unique characters.
     """
     if minimum_set is None:
         minimum_set = []
 
     complete_str = "".join(minimum_set + dataset)
+    if not include_whitespace:
+        complete_str = re.sub(r"\s", "", complete_str)
     characters = list(set(complete_str))
     return sorted(characters)
--- test/features/test_strings.py
+++ test/features/test_strings.py
@@ -19,3 +19,13 @@ def test_character_vocabulary_from_dataset_empty_dataset():
 def test_character_vocabulary_from_dataset_minimum():
     chars = character_vocabulary_from_dataset(["aaa"], minimum_set=["c", "b", "a"])
     assert chars == ["a", "b", "c"]
+
+
+def test_character_vocabulary_from_dataset_whitespace():
+    chars = character_vocabulary_from_dataset(["a b c"])
+    assert chars == ["a", "b", "c"]
+
+
+def test_character_vocabulary_from_dataset_include_whitespace():
+    chars = character_vocabulary_from_dataset(["a b\tc\n"], include_whitespace=True)
+    assert chars == ["\t", "\n", " ", "a", "b", "c"]
